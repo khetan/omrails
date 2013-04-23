dc111bd279e1f426bd535c25f9b7612a0b7c60e5
Khetan Sewpaul <khetan@sai.co.za>
2013-04-23 17:33:03 +0200

    added new mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Omrails::Application.configure do
 
 config.action_mailer.raise_delivery_errors = true
 config.action_mailer.perform_deliveries = true
-config.action_mailer.default_url_options = {:host => 'http://stark-falls-3858.herokuapp.com'}
+config.action_mailer.default_url_options = {:host => 'localhost:3000'}
 
 ActionMailer::Base.smtp_settings = {
 
