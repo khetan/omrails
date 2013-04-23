ea209e8ca332cd6cd8083104cd0cc86cd771ef7d
Khetan Sewpaul <khetan@sai.co.za>
2013-04-23 14:09:15 +0200

    Added forms

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ group :assets do
   gem 'coffee-rails', '~> 3.2.1'
   gem 'bootstrap-sass', '~> 2.3.1.0'
 
+
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
   # gem 'therubyracer', :platforms => :ruby
 
@@ -28,6 +29,8 @@ group :assets do
 end
 
 gem 'jquery-rails'
+gem 'devise'
+gem 'simple_form'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,9 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
+
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
@@ -58,5 +61,8 @@ module Omrails
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+   config.assets.initialize_on_precompile = false
+
   end
 end
